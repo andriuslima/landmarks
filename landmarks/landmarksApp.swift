0a977022febf9e19df309bda644c5776d6b08019
Andrius Lima <andrius.lima@townsq.com.br>
2025-08-23 12:42:25 -0400

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Andrius Lima on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ landmarksTests/landmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksTests.swift
+//  landmarksTests
+//
+//  Created by Andrius Lima on 8/23/25.
+//
+
+import Testing
+@testable import landmarks
+
+struct landmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
